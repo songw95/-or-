bbe3c689895453da3acde1fafbdd9268a1ccd7f6
송원준 <64256313+songw95@users.noreply.github.com>
2022-10-03 13:47:26 +0900

    Delete 1712.js

--- 1712.js
+++
@@ -1,9 +0,0 @@
-var input = require('fs').readFileSync('dev/stdin').toString().split(' ');
-var a = input[0];
-var b = input[1];
-var c = input[2];
-if (c - b <= 0) { // 수익이 0 이거나 -마이너스 일때.  -일때만 하면 틀림.
-    console.log(-1);
-} else {
-    console.log(Math.floor(a / (c - b) + 1));
-}
