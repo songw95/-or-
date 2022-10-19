bf4f40a4605e251ac4bf103044ac9c0f71e3e576
송원준 <64256313+songw95@users.noreply.github.com>
2022-10-19 10:35:09 +0900

    Add files via upload
    `` 말고 '' 로

---
+++ 5339.js
@@ -0,0 +1,24 @@
+/*     \ 는 \\ 로
+   틀린이유 - ``백틱 으로 사용하니 틀림. 
+
+
+
+   /  _  \
+  //|/.\|\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |
+
+*/
+console.log('     /~\\')
+console.log('    ( oo|')
+console.log('    _\\=/_')
+console.log('   /  _  \\')
+console.log('  //|/.\\|\\\\')
+console.log(' ||  \\ /  ||')
+console.log('============')
+console.log('|          |')
+console.log('|          |')
+console.log('|          |')
